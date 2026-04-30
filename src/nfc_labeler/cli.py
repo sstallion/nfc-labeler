06390e5d918826b064a58105b8c58b7012019310
Steven Stallion <sstallion@gmail.com>
2026-04-30 08:45:06 -0500

    Initial commit

---
+++ src/nfc_labeler/cli.py
@@ -0,0 +1,10 @@
+# Copyright 2026 Steven Stallion
+# SPDX-License-Identifier: BSD-2-Clause
+
+import click
+
+
+@click.group()
+def cli():
+    """Generates 3D printer filament labels for Santek EZ Sign displays"""
+    pass
---
+++ tests/test_cli.py
@@ -0,0 +1,6 @@
+# Copyright 2026 Steven Stallion
+# SPDX-License-Identifier: BSD-2-Clause
+
+
+def test_empty():
+    pass
